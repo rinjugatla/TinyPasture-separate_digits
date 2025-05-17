6b1b1c8c6506ddb009e2da3220bbc6b806e8006e
rinjugatla <rin.jugatla@gmail.com>
2025-05-18 01:03:34 +0900

    feat: ゲームアップデートで桁区切りされた場合の例外処理を追加

--- digits_utility.gd
+++ digits_utility.gd
@@ -13,6 +13,11 @@ static func format_float_with_commas(number: float) -> String:
 
 ## 桁区切りされていない数値を桁区切り
 static func format_number_text_with_commas(number_text: String) -> String:
+	# ゲームアップデートで桁区切りに対応した場合に二重で桁区切りしないようチェック
+	var has_comma = number_text.find(",") > -1
+	if has_comma:
+		return number_text
+
 	var formated = str(number_text)
 	var length = formated.length()
 	while length > 3:
